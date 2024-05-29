84e8c105990c8d0bf34db267eb6d09cf0c4eef3f
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-05-28 20:24:55 -0500

    using fetch function

---
+++ fetchApi.js
@@ -0,0 +1,16 @@
+fetch("https://swapi.dev/api/people/1/")
+.then((res) => {
+    console.log("Resolved!", res);
+    return res.json();
+})
+.then((data) => {
+    console.log(data);
+    return fetch("https://swapi.dev/api/people/2/")
+})
+.then((res) => {
+    console.log("Second request resolved!");
+    return res.json();
+})
+.catch((e) => {
+    console.log("Error!",e)
+})
